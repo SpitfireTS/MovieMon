95967225742a63a920523cba200c96f94746ac7c
Danny Alcantara <danny.alcantara@gmail.com>
2012-03-31 15:19:17 -0500

    small refactoring to provider

---
+++ MovieMon.Api/GetExtensions/TitleExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using MovieMon.Api.Models;
+using Netflix.Catalog.v2;
+
+namespace MovieMon.Api.GetExtensions
+{
+    public static class TitleExtensions
+    {
+
+        public static IEnumerable<AvailabilityInfo> GetAvailability(this Title title)
+        {
+            var availability = new List<AvailabilityInfo>();
+
+            if (title.Instant.Available)
+            {
+                availability.Add(new AvailabilityInfo
+                {
+                    AvailableFrom = title.Instant.AvailableFrom,
+                    AvailableTo = title.Instant.AvailableTo,
+                    MediaType = "Instant",
+                    ProviderName = "Netflix"
+                });
+            }
+
+            if (title.BluRay.Available)
+            {
+                availability.Add(new AvailabilityInfo
+                {
+                    AvailableFrom = title.BluRay.AvailableFrom,
+                    AvailableTo = title.BluRay.AvailableTo,
+                    MediaType = "BlueRay",
+                    ProviderName = "Netflix"
+                });
+            }
+
+            if (title.Dvd.Available)
+            {
+                availability.Add(new AvailabilityInfo
+                {
+                    AvailableFrom = title.Dvd.AvailableFrom,
+                    AvailableTo = title.Dvd.AvailableTo,
+                    MediaType = "Dvd",
+                    ProviderName = "Netflix"
+                });
+            }
+            return availability;
+        }
+
+        public static string GetRunTimeInMinutes(this Title title)
+        {
+            var runtime = title.Runtime;
+            if (runtime.HasValue)
+            {
+                var i = runtime.Value / 60;
+                return string.Format("{0} minutes.", i.ToString(CultureInfo.InvariantCulture));
+            }
+            return "0";
+        }
+
+        public static List<string> GetCast(this Title title)
+        {
+            var cast = new List<string>();
+
+            if (title.Cast.Any())
+            {
+                cast = title.Cast.Select(c => c.Name).ToList();
+            }
+            return cast;
+
+        }
+    }
+}
--- MovieMon.Api/Services/NetflixMovieProvider.cs
+++ MovieMon.Api/Services/NetflixMovieProvider.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Web;
+using MovieMon.Api.GetExtensions;
 using MovieMon.Api.Models;
 using Netflix.Catalog.v2;
 
@@ -24,18 +25,18 @@ namespace MovieMon.Api.Services
             movies = titlesList.Select(m => new Movie
                                                 {
                                                     ProviderMovieId = m.Id, 
-                                                    Availability = GetAvailability(m),
+                                                    Availability = m.GetAvailability(),
                                                     WatchedDate = null,
                                                     Title = m.Name,
-                                                    Cast = GetCast(m),
+                                                    Cast = m.GetCast(),
                                                     Summary = m.Synopsis,
-                                                    RunTime = GetRunTimeInMinutes(m.Runtime),
-                                                    Providers = new List<MovieProvider>{NETFLIX_DVD, NETFLIX_BLUERAY, NETFLIX_STREAM}
+                                                    RunTime = m.GetRunTimeInMinutes(),
+                                                    Providers = new List<MovieProvider> {NETFLIX_DVD, NETFLIX_BLUERAY, NETFLIX_STREAM}
                                                 }).ToList();
             return movies;
         }
 
-        private IEnumerable<Title> DoSearch(NetflixCatalog context, MovieSearchCriteria criteria)
+        private static IEnumerable<Title> DoSearch(NetflixCatalog context, MovieSearchCriteria criteria)
         {
             var results = new List<Title>();
 
@@ -46,67 +47,6 @@ namespace MovieMon.Api.Services
             }
 
             return results;
-        }
-
-        private IEnumerable<AvailabilityInfo> GetAvailability(Title title)
-        {
-            var availability = new List<AvailabilityInfo>();
-            
-            if(title.Instant.Available)
-            {
-                availability.Add(new AvailabilityInfo
-                                     {
-                                         AvailableFrom = title.Instant.AvailableFrom,
-                                         AvailableTo = title.Instant.AvailableTo,
-                                         MediaType = "Instant",
-                                         ProviderName = "Netflix"
-                                     });
-            }
-
-            if (title.BluRay.Available)
-            {
-                availability.Add(new AvailabilityInfo
-                {
-                    AvailableFrom = title.BluRay.AvailableFrom,
-                    AvailableTo = title.BluRay.AvailableTo,
-                    MediaType = "BlueRay",
-                    ProviderName = "Netflix"
-                });
-            }
-
-            if (title.Dvd.Available)
-            {
-                availability.Add(new AvailabilityInfo
-                {
-                    AvailableFrom = title.Dvd.AvailableFrom,
-                    AvailableTo = title.Dvd.AvailableTo,
-                    MediaType = "Dvd",
-                    ProviderName = "Netflix"
-                });
-            }
-            return availability;
-        }
-
-        private string GetRunTimeInMinutes(int? runtime)
-        {
-            if(runtime.HasValue)
-            {
-                var i = runtime.Value/60;
-                return string.Format("{0} minutes.", i.ToString(CultureInfo.InvariantCulture));
-            }
-            return "0";
-        }
-
-        private List<string> GetCast(Title title)
-        {
-            var cast = new List<string>();
-            
-            if (title.Cast.Any())
-            {
-                cast = title.Cast.Select(c => c.Name).ToList();
-            }
-            return cast;
-
         }
     }
 }
