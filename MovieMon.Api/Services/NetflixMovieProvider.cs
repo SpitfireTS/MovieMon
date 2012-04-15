9f575cf6f9d9c95f6aff6ee52d1d8628812617e7
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-14 23:34:29 -0500

    Integration with rotten tomatoes

--- MovieMon.Api/Services/NetflixMovieProvider.cs
+++ MovieMon.Api/Services/NetflixMovieProvider.cs
@@ -16,31 +16,28 @@ namespace MovieMon.Api.Services
         {
             var netflixUri = new Uri("http://odata.netflix.com/Catalog/");
             var context = new NetflixCatalog(netflixUri);
-            var movies = new List<Movie>();
 
             var titlesList = DoSearch(context, criteria);
-            movies = titlesList.Select(m => new Movie
-                                                {                                                   
-                                                    ProviderMovieId = m.NetflixApiId, 
-                                                    Availability = m.GetAvailability(),
-                                                    WatchedDate = null,
-                                                    Title = m.Name,
-                                                    Cast = m.GetCast(),
-                                                    Summary = m.Synopsis,
-                                                    RunTime = m.GetRunTimeInMinutes(),
-                                                    RelatedImages = m.GetRelatedImages(),
-                                                    MPAARating = m.Rating
+            var movies = titlesList.Select(m => new Movie
+                                                     {                                                   
+                                                         ProviderMovieId = m.Id, 
+                                                         Availability = m.GetAvailability(),
+                                                         WatchedDate = null,
+                                                         Title = m.Name,
+                                                         Cast = m.GetCast(),
+                                                         Summary = m.Synopsis,
+                                                         RunTime = m.GetRunTimeInMinutes(),
+                                                         RelatedImages = m.GetRelatedImages(),
+                                                         MPAARating = m.Rating
                                                     
                                            
-                                                }).ToList();
+                                                     }).ToList();
             return movies;
         }
 
         private static IEnumerable<Title> DoSearch(NetflixCatalog context, MovieSearchCriteria criteria)
         {
-            var results = new List<Title>();
-
-            results = context.Titles.Where(t => t.Name.Equals(criteria.Title)).Select(t => t).ToList();
+            var results = context.Titles.Where(t => t.Name.Equals(criteria.Title)).Select(t => t).ToList();
             if (!results.Any())
             {
                 results = context.Titles.Where(t => t.Name.Contains(criteria.Title)).Select(t => t).ToList();
