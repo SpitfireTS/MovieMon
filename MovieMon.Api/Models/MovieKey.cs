861a28e2ce9dcaae2c2e1d34bb6e7550f6a48f04
Danny Alcantara <danny.alcantara@gmail.com>
2012-04-15 01:26:36 -0500

    new files

---
+++ MovieMon.Api/Models/MovieKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieMon.Api.Models
+{
+    public class MovieKey
+    {
+        public string NetflixId { get; set; }
+        public string RottenTomatoesId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ MovieMon.Api/Services/RottenTomatoesProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieMon.Api.Models;
+
+namespace MovieMon.Api.Services
+{
+    public class RottenTomatoesProvider:IMovieProvider
+    {
+        public IEnumerable<Movie> SearchMovies(MovieSearchCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
