bcd1c08112aadfe0736c33bc87c48c7818e94bab
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-25 15:09:14 -0500

    Added tests, added support for rating movies

---
+++ MovieMon.Api/Controllers/UrlHelperWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace MovieMon.Api.Controllers
+{
+    public class UrlHelperWrapper
+    {
+        public string Route(string routeName, object o, UrlHelper url)
+        {
+            return url.Route(routeName, o);
+        }
+    }
+}
