75d7168349917310bd603d5b66391e611db3ea27
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-25 22:19:07 -0500

    added RedBox!! yeah right!!

---
+++ MovieMon.Api/Models/MovieReservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieMon.Api.Models
+{
+    public class MovieReservation
+    {
+        public string ProviderName { get; set; }
+        public string Title { get; set; }
+        public string Format { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+    }
+
+}
---
+++ MovieMon.Api/Models/MovieReservationResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieMon.Api.Models
+{
+    public class MovieReservationResult
+    {
+        public string ConfirmationMessage { get; set; }
+        
+    }
+}
