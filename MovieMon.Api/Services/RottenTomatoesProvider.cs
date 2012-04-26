d56705b636f51a065baa8e167e94e8fc87ad3d2b
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-25 21:16:58 -0500

    integrated rotten tomatoes with member queue

--- MovieMon.Api/Services/RottenTomatoesProvider.cs
+++ MovieMon.Api/Services/RottenTomatoesProvider.cs
@@ -9,21 +9,23 @@ using System.Threading.Tasks;
 using System.Web;
 using MovieMon.Api.Models;
 using Newtonsoft.Json;
+using RottenTomatoes.Types.MovieInfo;
 using Clips=RottenTomatoes.Types.Clips;
 using Cast=RottenTomatoes.Types.Cast;
 using Reviews=RottenTomatoes.Types.Reviews;
 using MovieInfo=RottenTomatoes.Types.MovieInfo;
 using RTMovie = RottenTomatoes.Types.Movie;
+
 using MovieMon.Api.Extensions;
 
 
 namespace MovieMon.Api.Services
 {
-    public class RottenTomatoesProvider:IMovieProvider
+    public class RottenTomatoesProvider : IMovieProvider
     {
         private static readonly string KEY = "xfnx2xp2tqc7mpbqmx3jet3k";
         private static readonly string MOVIE_SEARCH_URL = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xfnx2xp2tqc7mpbqmx3jet3k&q={0}";
-
+        private static readonly string MOVIE_INFO_URL = "http://api.rottentomatoes.com/api/public/v1.0/movies/{0}.json?apikey=xfnx2xp2tqc7mpbqmx3jet3k";
         public RottenTomatoesProvider()
         {
             Name = "Rotten Tomatoes";
@@ -31,96 +33,44 @@ namespace MovieMon.Api.Services
 
         public IEnumerable<Movie> SearchMovies(MovieSearchCriteria criteria)
         {
-            if (string.IsNullOrWhiteSpace(criteria.Title)) return new List<Movie>();
-
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));            
-            var movieUrl = string.Format(MOVIE_SEARCH_URL, criteria.Title);
-            
-            client.BaseAddress = new Uri(movieUrl);
-            var searchResult = client.GetStringAsync("").Result;
-            var rottenTomatoesMovieList = JsonConvert.DeserializeObject<RTMovie.RootObject>(searchResult);
-
-            var filteredList =
-                rottenTomatoesMovieList.movies.Where(
-                    m => m.title.Equals(criteria.Title, StringComparison.OrdinalIgnoreCase)).ToList();
-
-            if (!filteredList.Any())
+            if (!ShouldSearch(criteria))
             {
-                filteredList = rottenTomatoesMovieList.movies.Where(m => m.title.ToLower().Contains(criteria.Title.ToLower())).ToList();
+                return new List<Movie>();
             }
-
-
-            return (from movie in filteredList
-                    let review = GetReviews(movie.id)
-                    let clips = GetClips(movie.id)
-                    let cast = GetCast(movie.id)
-                    select new Movie
-                               {
-                                   Cast = cast.ToList(),
-                                   RelatedClips = clips.ToList(),
-                                   Reviews = review.ToList(),
-                                   ProviderMovieId = movie.id,
-                                   Title = movie.title,
-                                   Summary = movie.synopsis,
-                                   RunTime = movie.runtime,
-                                   Availability = movie.GetAvailability(),
-                                   RelatedImages = movie.GetRelatedImages(),
-                                   MPAARating = movie.mpaa_rating,
-                                   Source = "Rotten Tomatoes",
-                                   Rating = movie.Ratings()
-                                   
-                               }).ToList();
-
-                                      //ProviderMovieId = m.Id, 
-                                      //                   Availability = m.GetAvailability(),
-                                      //                   WatchedDate = null,
-                                      //                   Title = m.Name,
-                                      //                   Cast = m.GetCast(),
-                                      //                   Summary = m.Synopsis,
-                                      //                   RunTime = m.GetRunTimeInMinutes(),
-                                      //                   RelatedImages = m.GetRelatedImages(),
-                                      //                   MPAARating = m.Rating
-
-//            Parallel.ForEach(filteredList, movie =>
-//                                               {
-//
-//                                                   var review = GetReviews(movie.id);
-//                                                   var clips = GetClips(movie.id);
-//                                                   var cast = GetCast(movie.id);
-//                                                   movieAttribute.TryAdd(movie.id,
-//                                                                     new MoviePartMap
-//                                                                         {
-//                                                                             Title = movie.title,
-//                                                                             Cast = cast.ToList(),
-//                                                                             Clips = clips.ToList(),
-//                                                                             Reviews = review.ToList()
-//                                                                         });
-//                                               });
             
-           
+
+            var movies = !string.IsNullOrWhiteSpace(criteria.Title) ? SearchByName(criteria.Title).ToList() : SearchByKey(criteria.Key).ToList();
+
+            return movies;
+        }
+
+        private static bool ShouldSearch(MovieSearchCriteria criteria)
+        {
+            if (!string.IsNullOrWhiteSpace(criteria.Title)) return true;
+
+            return (criteria.Key != null && !string.IsNullOrWhiteSpace(criteria.Key.RottenTomatoesId));
         }
 
         public string Name { get; set; }
 
         public IEnumerable<string> GetCast(string movieId)
-        {            
+        {
             var movieUrl = movieId.CastUrl();
             var cast = GetMovieAttribute<Cast.RootObject>(movieUrl);
             var casts = new List<string>();
-            if (casts!=null && cast.cast!=null)
+            if (casts != null && cast.cast != null)
             {
                 casts = cast.cast.Take(5).Select(c => c.name).ToList();
-            }            
+            }
             return casts;
         }
 
         public IEnumerable<Review> GetReviews(string movieId)
-        {            
-            var reviewsUrl = movieId.ReviewsUrl();            
+        {
+            var reviewsUrl = movieId.ReviewsUrl();
             var reviewsPart = GetMovieAttribute<Reviews.RootObject>(reviewsUrl);
             var reviews = new List<Review>();
-            if (reviewsPart!=null && reviewsPart.reviews!=null)
+            if (reviewsPart != null && reviewsPart.reviews != null)
             {
                 reviews = reviewsPart.reviews.Select(
                     r =>
@@ -141,7 +91,7 @@ namespace MovieMon.Api.Services
             var clipsUrl = movieId.ClipsUrl();
             var clipPart = GetMovieAttribute<Clips.RootObject>(clipsUrl);
             var clips = new List<string>();
-            if (clipPart!=null && clipPart.clips!=null)
+            if (clipPart != null && clipPart.clips != null)
             {
                 clips = clipPart.clips.Select(c => c.links.alternate).ToList();
             }
@@ -168,5 +118,65 @@ namespace MovieMon.Api.Services
             return new T();
         }
 
+        private IEnumerable<Movie> SearchByName(string title)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var movieUrl = string.Format(MOVIE_SEARCH_URL, title);
+            client.BaseAddress = new Uri(movieUrl);
+            var searchResult = client.GetStringAsync("").Result;
+            var rottenTomatoesMovieList = JsonConvert.DeserializeObject<RTMovie.RootObject>(searchResult);
+
+            var filteredList =
+                rottenTomatoesMovieList.movies.Where(
+                    m => m.title.Equals(title, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (!filteredList.Any())
+            {
+                filteredList = rottenTomatoesMovieList.movies.Where(m => m.title.ToLower().Contains(title.ToLower())).ToList();
+            }
+
+
+            return (from movie in filteredList
+                    let review = GetReviews(movie.id)
+                    let clips = GetClips(movie.id)
+                    let cast = GetCast(movie.id)
+                    select new Movie
+                    {
+                        Cast = cast.ToList(),
+                        RelatedClips = clips.ToList(),
+                        Reviews = review.ToList(),
+                        ProviderMovieId = movie.id,
+                        Title = movie.title,
+                        Summary = movie.synopsis,
+                        RunTime = movie.runtime,
+                        Availability = movie.GetAvailability(),
+                        RelatedImages = movie.GetRelatedImages(),
+                        MPAARating = movie.mpaa_rating,
+                        Source = "Rotten Tomatoes",
+                        Rating = movie.Ratings()
+
+                    }).ToList();
+        }
+
+        private IEnumerable<Movie> SearchByKey(MovieKey key)
+        {
+            var movies = new List<Movie>();
+
+            if (string.IsNullOrWhiteSpace(key.RottenTomatoesId))
+                return movies; // we have no id return an empty list so we don't fail.
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var movieUrl = string.Format(MOVIE_INFO_URL, key.RottenTomatoesId);
+            client.BaseAddress = new Uri(movieUrl);
+            var searchResult = client.GetStringAsync("").Result;
+            var rottenMovie = JsonConvert.DeserializeObject<RootObject>(searchResult);
+            var name = rottenMovie.title;
+            return SearchByName(name);            
+        }
+            
     }
 }
