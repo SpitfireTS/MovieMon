238db5e9ed32b8063d5aa17dfbdc61379c8d9431
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-30 20:46:16 -0500

    Added a second movie to default member's queue

--- MovieMon.Api/Data/InMemoryMemberRepo.cs
+++ MovieMon.Api/Data/InMemoryMemberRepo.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using MovieMon.Api.Models;
 
 namespace MovieMon.Api.Data
 {
-    public class InMemoryMemberRepo:IMemberRepository
+    public class InMemoryMemberRepo : IMemberRepository
     {
-        private static IList<Member> _members;
+        private static readonly IList<Member> _members;
+
         static InMemoryMemberRepo()
         {
             _members = new List<Member>
@@ -26,27 +26,42 @@ namespace MovieMon.Api.Data
                                                             {
                                                                 Title = "Rocky",
                                                                 Key = new MovieKey
-                                                                        {
-                                                                            NetflixId = "82Q3",
-                                                                            RottenTomatoesId = "11405",
-                                                                            wasWatched = false,
-                                                                            IsInQueue = true
-                                                                        }
+                                                                          {
+                                                                              NetflixId = "82Q3",
+                                                                              RottenTomatoesId = "11405",
+                                                                              wasWatched = false,
+                                                                              IsInQueue = true
+                                                                          }
+                                                            },
+                                                        new Movie
+                                                            {
+                                                                Title = "The Godfather",
+                                                                Key = new MovieKey
+                                                                          {
+                                                                              NetflixId = "ApOD2",
+                                                                              RottenTomatoesId = "12911",
+                                                                              wasWatched = true,
+                                                                              IsInQueue = true,
+                                                                              Comment =
+                                                                                  "This was a great movie. In scene after scene ... Coppola crafted an enduring, undisputed masterpiece",
+                                                                              Rating = 4
+                                                                          }
                                                             }
                                                     }
                                    },
                            };
-
         }
-        
+
+        #region IMemberRepository Members
+
         public IEnumerable<Member> GetAll()
-        {            
+        {
             return _members;
         }
 
         public Member GetById(Guid id)
         {
-            var member  = _members.FirstOrDefault(m => m.Id == id);
+            Member member = _members.FirstOrDefault(m => m.Id == id);
             return member;
         }
 
@@ -56,8 +71,8 @@ namespace MovieMon.Api.Data
             {
                 member.Id = Guid.NewGuid();
             }
-            
-            if (member.Movies==null)
+
+            if (member.Movies == null)
             {
                 member.Movies = new List<Movie>();
             }
@@ -73,13 +88,15 @@ namespace MovieMon.Api.Data
 
         public bool Update(Member member)
         {
-            var found = _members.FirstOrDefault(m => m.Id == member.Id);
-            if (found!=null)
+            Member found = _members.FirstOrDefault(m => m.Id == member.Id);
+            if (found != null)
             {
-                var index = _members.IndexOf(found);
+                int index = _members.IndexOf(found);
                 _members[index] = member;
             }
             return true;
         }
+
+        #endregion
     }
 }
