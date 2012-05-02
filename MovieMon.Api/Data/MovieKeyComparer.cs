5a3f99e08a09dab85d8caa0ee49be6eef737af2c
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-05-01 20:54:38 -0500

    Added fix for duplicate movies on queue.  fixed unit tests

---
+++ MovieMon.Api/Data/MovieKeyComparer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MovieMon.Api.Models;
+
+namespace MovieMon.Api.Data
+{
+    public class MovieKeyComparer : IEqualityComparer<Movie>
+    {
+        public bool Equals(Movie x, Movie y)
+        {
+            if(x.Key!=null && y.Key!=null)
+            {
+                return x.Key.NetflixId == y.Key.NetflixId;
+            }
+            return (x.Key==y.Key);
+        }
+
+        public int GetHashCode(Movie obj)
+        {
+            return obj.Key.NetflixId.GetHashCode();
+        }
+    }
+}
